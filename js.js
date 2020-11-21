783d72ef656635dd267245cc92abcf5da8bc7962
abb <abb@step.lv>
2020-11-21 17:38:55 +0200

    Work in univer

--- js.js
+++ js.js
@@ -3,3 +3,16 @@ $(document).ready(function() {
   });
 
 $('select').material_select('destroy');
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
+ 
+// Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
